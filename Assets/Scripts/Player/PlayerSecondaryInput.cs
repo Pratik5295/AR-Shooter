73c725ff06d3f43a8e582cc48bcfd8a2b954c1f7
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2025-01-28 20:22:11 +0000

    Secondary input for bullet spawning

---
+++ Assets/Scripts/Player/PlayerSecondaryInput.cs
@@ -0,0 +1,39 @@
+using StarterAssets;
+using UnityEngine;
+
+public class PlayerSecondaryInput : MonoBehaviour
+{
+    private StarterAssetsInputs _input;
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private Transform bulletSpawnPoint;
+
+    private void Start()
+    {
+        _input = GetComponent<StarterAssetsInputs>();
+    }
+
+    private void Update()
+    {
+        //Check for shooting
+
+        Shooting();
+    }
+
+    private void Shooting()
+    {
+        if (_input != null)
+        {
+            if (_input.shoot)
+            {
+                //Instantiate the shooting projectile
+                Instantiate(bulletPrefab,bulletSpawnPoint.position, Quaternion.identity);
+
+                _input.shoot = false;
+            }
+        }
+    }
+}
