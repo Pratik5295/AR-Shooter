4fbc505de586e11b2bc4978eb1161d0af607a714
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2025-01-28 19:57:19 +0000

    Moving the character in forward direction

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
     public enum GameState
@@ -20,6 +21,8 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private GameObject PlayerObject;
+
+    public TextMeshProUGUI moveText;
 
     public Action<GameState> OnStateChanged;
 
